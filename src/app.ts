3016c604c15c3dbf599e94a3e6def76e166b7123
Anjishnu Banerjee <inviktor27@gmail.com>
2025-08-01 15:19:29 +0530

    Add routes to main app

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 import express from "express";
 import { Request, Response } from "express";
+import analyticsRouter from "./routes/analyticsRouter";
+import alertRouter from "./routes/alertRouter";
+import telemetryRouter from "./routes/telemetryRouter";
+import vehicleRouter from "./routes/vehicleRouter";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Hello", message1: "World" });
-});
+app.use("/analytics", analyticsRouter);
+app.use("/vehicle", vehicleRouter);
+app.use("/telemetry", telemetryRouter);
+app.use("/alert", alertRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
